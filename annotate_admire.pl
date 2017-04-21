dcd2d0ff9878cee35c3dc45c695e1d4b9e100348
nroak <ninadoak@gmail.com>
2017-04-21 14:34:12 -0500

    Add files via upload

---
+++ annotate_admire.pl
@@ -0,0 +1,124 @@
+=head1 LICENSE
+Copyright [2017] [Ninad Oak]										
+													
+Licensed under the Apache License, Version 2.0 (the "License");						
+you may not use this file except in compliance with the License.					
+You may obtain a copy of the License at									
+													
+    http://www.apache.org/licenses/LICENSE-2.0								
+													
+Unless required by applicable law or agreed to in writing, software					
+distributed under the License is distributed on an "AS IS" BASIS,					
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.				
+See the License for the specific language governing permissions and					
+limitations under the License.										
+
+=cut
+
+=head2 USAGE
+
+Download ADmiRE.tab from: https://figshare.com/s/7b3bea2aa33470396f50
+
+Run the script: 
+perl annotate_admire.pl [--input FILE] [--output FILE] [--admire_path=PATH] [--chr=NUMBER] [--pos=NUMBER]
+--input 		  = Tab separated input file
+--output [OPTIONAL]	  = 17 columns from admire database are added to the end of each row, if no information is available, 'NA's are populated, 
+		    	    Default file name, $input.admire.tab
+--admire_path [OPTIONAL]  = File location to ADmiRE.tab file. Default is the same directory as the code.
+--chr 			  = Input the COLUMN number with chromosome information from the tab separated file
+--pos 			  = Input the COLUMN number with position information from the tab separated file
+
+
+=cut
+
+=head1 HEADERS
+ADmiRE Headers												
+   1  MIRNA												
+   2  PRE_ID												
+   3  MIRNA_Feature											
+   4  Family_Name											
+   5  Family_ID												
+   6  Mature_Pos											
+   7  Mature_Name											
+   8  Mature_ID												
+   9  Mature_Type											
+  10  High_Confidence											
+  11  gnomAD_Constrained_MIRNA										
+  12  Conserved_ADmiRE											
+  13  HMDD_Disease (PMID)										
+  14  "HMDD_Targets (Disease, PMID)"									
+  15  miRTarBase_#Validated Targets									
+  16  miRTarBase_TargetGene(Validation type)								
+  17  miRTarBase_Reference(PMID)									
+
+=cut
+
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Getopt::Long qw(GetOptions);
+use Pod::Usage;
+
+$"="\t";
+my $input;
+my $output;
+my $admire_path= './';
+my $chr = "0";
+my $pos = "1";
+my $help;
+
+GetOptions ("input|i=s" => \$input,    # string
+            "output|o:s"   => \$output,      # string
+            "admire_path|p:s"  => \$admire_path, # string
+            "chr|c:i" => \$chr,    # numeric
+            "pos|c:i" => \$pos,    # numeric
+            'help|h' => \$help)  # flag
+or die("Usage: perl annotate_admire.pl [--input FILE] [--output FILE] [--admire_path=PATH] [--chr=NUMBER] [--pos=NUMBER]\n");
+
+if (defined $input) {
+    } else {
+    	print "Option input requires an argument
+	Usage: perl annotate_admire.pl [--input FILE] [--output FILE] [--admire_path=PATH] [--chr=NUMBER] [--pos=NUMBER]\n";
+	exit 1
+}
+if (defined $output) {
+	} else {
+    	$output=$input.".admire.tab";
+}
+
+##### READ ADMIRE INTO HASH	
+my $input_db= $admire_path."ADmiRE.tab";
+my %mirbase;
+open FD, "<$input_db" or die "No Database";
+foreach my $line_db (<FD>){
+	$line_db =~ s/[\r\n]//g;
+	my @entry_db = split (/\t/,$line_db);
+	my $hash_key1=$entry_db[0]."_".$entry_db[1];
+	my $hash_key2=$entry_db[3];
+	my @hash_value= @entry_db[0..$#entry_db];	
+	if ($entry_db[0] ne "MIRNA" ){push (@{$mirbase{$hash_key1}{$hash_key2}}, @hash_value);}
+	}
+close FD;
+
+## READ VARIANT FILE AND ANNOTATE 
+open FP, "<$input" or die "No Variant File";
+open FO, ">$output" or die "No Output File";
+
+my @empty;
+push @empty, "NA" foreach(1..17);
+
+ALLMUT: while (my $line_maf = <FP>){
+	$line_maf =~ s/\r+\n$//g;
+	#print $line_maf."\n";
+	my @entry_maf = split ("\t",$line_maf);
+	chomp @entry_maf;
+	if ($line_maf =~ /^\#/ || $line_maf =~ /^CHROM/) { print FO "@entry_maf\tMIRNA\tPRE_ID\tMIRNA_Feature\tFamily_Name\tFamily_ID\tMature_Pos\tMature_Name\tMature_ID\tMature_Type\tHigh_Confidence\tgnomAD_Constrained_MIRNA\tConserved_100vert_ADmiRE\tHMDD_Disease (PMID)\tHMDD_Targets (Disease, PMID)\tmiRTarBase_#Validated Targets\tmiRTarBase_TargetGene(Validation type)\tmiRTarBase_Reference(PMID)\n";}
+	elsif ($line_maf !~ /^\#/ && $line_maf !~ /^CHROM/) {
+		my $db_key=$entry_maf[$chr]."_".$entry_maf[$pos];
+		if (!defined %{$mirbase{$db_key}}){print FO "@entry_maf\t@empty\n";next ALLMUT;}
+		foreach my $db_key2 ( keys %{$mirbase{$db_key}}) {
+			print FO "@entry_maf\t@{$mirbase{$db_key}{$db_key2}}[3..19]\n";
+		   	}
+		next ALLMUT;
+		}
+	}
